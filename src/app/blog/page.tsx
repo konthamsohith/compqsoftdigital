34bde792eb42f7e84ffd5d80df9163d475a58a2f
konthamsohith <konthamsohith2@gmail.com>
2026-09-11 19:02:31 +0530

    fix dynamic pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,8 @@ import FooterSection from '@/components/FooterSection';
 import ScrollIndicator from '@/components/ScrollIndicator';
 import BlogGrid from '@/components/blog/BlogGrid';
 
+export const dynamic = 'force-dynamic';
+
 export default function BlogPage() {
   return (
     <main style={{ paddingTop: '80px' }}>
