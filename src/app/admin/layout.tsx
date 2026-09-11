33753448c68c42a0e5a1c693cfd5156962e516f3
konthamsohith <konthamsohith2@gmail.com>
2026-09-11 21:47:53 +0530

    feat: remove news from admin panel

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -22,13 +22,7 @@ const navLinks = [
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" /></svg>
     ),
   },
-  {
-    name: "News",
-    href: "/admin/news",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" /></svg>
-    ),
-  },
+
   {
     name: "Events",
     href: "/admin/events",
